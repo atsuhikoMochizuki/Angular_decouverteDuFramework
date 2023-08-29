7d14bc89c6173ca8186ded1af1cb91300dbdf60b
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-08-29 13:25:10 +0200

    création d'un module racine

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,57 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Component, NgModule } from "@angular/core";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppModule } from './app/app.module';
 
+@Component({
+  selector: 'app-root',
+  template: '<app-header></app-header><app-footer></app-footer>'
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+})
+class AppComponent { }
+
+@Component({
+  selector: 'app-header',
+  template: `
+  <h2>Haut de page</h2>
+  <app-menu></app-menu>>
+`
+})
+class HeaderComponent { }
+
+@Component({
+  selector: 'app-footer',
+  template: '<footer>Pied de page </footer>'
+})
+class FooterComponent { }
+
+@Component({
+  selector: 'app-menu',
+  template: `
+  <div><a href='/home'>Accueil</a></div>
+  <div>< a href = '/presentation'> Présentation < /a></div >
+  <div><a href='/contact' > Contact < /a></div >
+  `
+})
+class MenuComponent {
+
+}
+
+@NgModule({
+  declarations:[
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
+],
+  imports:[
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+class MyModule{
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule);
