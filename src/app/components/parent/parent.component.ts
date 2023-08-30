151d66ed04d477a8311fa624131bf5f9ebd37ef2
atsuhikoMochizuki <atsuhiko_mochizuki27@laposte.net>
2023-08-30 17:59:13 +0200

    TP établir com terminé

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
   export class ParentComponent {
-    test(event:string){
-      console.log("L'enfant vient d'emettre le message",event.value);
-    }
+    received = '';
+   
   }
